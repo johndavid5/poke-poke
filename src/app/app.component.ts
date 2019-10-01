1621c476818f52cfee70338bf9db7592b098ca36
John D. Aynedjian <johndavid5@yahoo.com>
2019-09-30 22:06:01 -0400

    starting out, more or less...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,16 @@ export class AppComponent implements OnInit {
     constructor(private apiService: ApiService) { }
 
     ngOnInit() {
-        this.apiService.getInventory().subscribe(i => this.menu = i);
+      this.apiService.getInventory()
+      .subscribe(
+      inventory => {
+        let sWho = "AppComponent::ngOnInit().this.apiService.getInventory().subscribe()"
+        this.menu = inventory
+        console.log("Got from ApiService: this.menu = inventory = ", this.menu )
+      });
     }
 
     get menuString(): string {
-        return JSON.stringify(this.menu);
+        return JSON.stringify(this.menu, null, ' ');
     }
 }
